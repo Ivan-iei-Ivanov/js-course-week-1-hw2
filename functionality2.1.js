2df07358596daaed64c3186382af6f753bdb1e9a
Ivan Ivanov <ivan.iei.ivanov@gmail.com>
2023-06-13 10:58:14 +0300

    Solution of hw2 up to functionality 2.1

---
+++ functionality2.1.js
@@ -0,0 +1,43 @@
+// var panCollection = prompt("Колко тигана има в кухнята?")
+// var potCollection = prompt("Колко тенджери има в кухнята?")
+// var kitchenWidth = prompt("Колко сантиметра е широка кухнята?")
+// var kitchenLength = prompt("Колко сантиметра е дълга кухнята?")
+// var kitchenHeigth = prompt("Колко сантиметра е висока кухнята?")
+// var chefName = prompt("Как се казва главният готвач?")
+// var cookCollection = prompt("Колко готвачи работят в кухнята?")
+// var waiterCollection = prompt("Колко сервитьори работят в ресторанта?")
+// var sauceResponsiblePersonName = prompt("Как се казва отговорника на сосовете?")
+// var kitchenMinTemperature = prompt("Каква е минималната температура в кухнята?")
+// var kitchenMaxTemperature = prompt("Каква е максималната температура в кухнята?")
+// var bankAccountAmount = prompt("Колко парички имате в банковата си сметка?")
+// var openingTime = prompt("В колко часа отваряте?")
+// var closingTime = prompt("В колко часа затваряте?")
+
+// // Optional questions, to be asked only if the user aknowledges his commitment to answer them
+// var optionalQuestionsQuery = prompt("Желаете ли да отговорите на допълнителни въпроси?") 
+// if (optionalQuestionsQuery === "да", "Да"){
+//   var isRefrigeratorOwned = prompt("Разполагате ли с хладилник?")
+//   var isGasStoveOwned = prompt("Разполагате ли с газов котлон?")
+//   var isConvectionOvenOwned = prompt("Разполагате ли с конвектомат?")
+//   var isElectricalGrillnOwned = prompt("Разполагате ли с електрическа скара?")
+//   var isKitchenHoodOwned = prompt("Разполагате ли с аспиратор?")
+// }
+// else {
+//   exit()
+// }
+
+do {foodDishType = prompt("Какво ястие желаете да ви приготвя?")
+    
+} while (!(foodDishType === "Месно ястие" || foodDishType === "Вегетарианско ястие" || foodDishType === "Десерт"))
+if (foodDishType === "Месно ястие"){
+    meatPortionSize = prompt("Каква порция по размер желаете?")
+    if (meatPortionSize === "малка порция"){
+        alert("Вие поръчахте малка порция от 450 грама, благодарим Ви за поръчката.")
+    } else if (meatPortionSize === "средна порция"){
+        alert("Вие поръчахте средна порция от 750 грама, благодарим Ви за поръчката.")
+    } else if (meatPortionSize === "голяма порция"){
+        alert("Вие поръчахте голяма порция от 950 грама, благодарим Ви за поръчката.")
+    } else {
+        alert("Няма такава порция.")
+    }
+    }
