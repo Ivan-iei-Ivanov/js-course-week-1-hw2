4210b303a6213c3b43a570c6620e0b873335145b
Ivan Ivanov <ivan.iei.ivanov@gmail.com>
2023-06-13 15:10:15 +0300

    complete solution of functionality 2.1

--- functionality2.1.js
+++ functionality2.1.js
@@ -1,74 +1,126 @@
-var panCollection = prompt("Колко тигана има в кухнята?")
-var potCollection = prompt("Колко тенджери има в кухнята?")
-var kitchenWidth = prompt("Колко сантиметра е широка кухнята?")
-var kitchenLength = prompt("Колко сантиметра е дълга кухнята?")
-var kitchenHeigth = prompt("Колко сантиметра е висока кухнята?")
-var headChefName = prompt("Как се казва главният готвач?")
-var chefs = prompt("Колко готвачи работят в кухнята?")
-var waiterCollection = prompt("Колко сервитьори работят в ресторанта?")
-var sauceChef = prompt("Как се казва отговорника на сосовете?")
-var kitchenMinTemperature = prompt("Каква е минималната температура в кухнята?")
-var kitchenMaxTemperature = prompt("Каква е максималната температура в кухнята?")
-var bankAccountAmount = prompt("Колко парички имате в банковата си сметка?")
-var openingTime = prompt("В колко часа отваряте?")
-var closingTime = prompt("В колко часа затваряте?")
+var fryingPanCollection = prompt("Колко тигана има в кухнята?");
+var potCollection = prompt("Колко тенджери има в кухнята?");
+var kitchenWidth = prompt("Колко сантиметра е широка кухнята?");
+var kitchenLength = prompt("Колко сантиметра е дълга кухнята?");
+var kitchenHeigth = prompt("Колко сантиметра е висока кухнята?");
+const KITCHEN_AREA = kitchenWidth * kitchenLength * kitchenHeigth;
+var headChefName = prompt("Как се казва главният готвач?");
+var chefs = prompt("Колко готвачи работят в кухнята?");
+var waiterCollection = prompt("Колко сервитьори работят в ресторанта?");
+var sauceChef = prompt("Как се казва отговорника на сосовете?");
+var kitchenMinTemperature = parseInt(
+  prompt("Каква е минималната температура в кухнята?")
+);
+var kitchenMaxTemperature = parseInt(
+  prompt("Каква е максималната температура в кухнята?")
+);
+var bankAccountAmount = prompt("Колко парички имате в банковата си сметка?");
+var openingTime = parseFloat(prompt("В колко часа отваряте?"));
+var closingTime = parseFloat(prompt("В колко часа затваряте?"));
 
 // Optional questions, to be asked only if the user aknowledges his commitment to answer them
-var optionalQuestionsQuery = prompt("Желаете ли да отговорите на допълнителни въпроси?") 
-if (optionalQuestionsQuery === "да", "Да"){
-  var isRefrigeratorOwned = prompt("Разполагате ли с хладилник?")
-  var isGasStoveOwned = prompt("Разполагате ли с газов котлон?")
-  var isConvectionOvenOwned = prompt("Разполагате ли с конвектомат?")
-  var isElectricalGrillnOwned = prompt("Разполагате ли с електрическа скара?")
-  var isKitchenHoodOwned = prompt("Разполагате ли с аспиратор?")
+var optionalQuestionsQuery = prompt(
+  "Желаете ли да отговорите на допълнителни въпроси?"
+);
+if (optionalQuestionsQuery === "да" || optionalQuestionsQuery === "Да") {
+  var hasRefrigerator = prompt("Разполагате ли с хладилник?");
+  var hassGasStove = prompt("Разполагате ли с газов котлон?");
+  var hasConvectionOven = prompt("Разполагате ли с конвектомат?");
+  var hasElectricGrill = prompt("Разполагате ли с електрическа скара?");
+  var hasKitchenHood = prompt("Разполагате ли с аспиратор?");
+} else {
+  exit();
 }
-else {
-  exit()
+
+do {
+  foodDishType = prompt(
+    "Какво ястие желаете да ви приготвя? Месно ястие, Вегетарианско ястие или Десерт"
+  );
+
+  const MEAL_TYPES = {
+    MEAT: "Месно ястие",
+    VEGETARIAN: "Вегетарианско ястие",
+    DESSERT: "Десерт",
+  };
+} while (
+  !(
+    foodDishType === MEAL_TYPES.MEAT ||
+    foodDishType === MEAL_TYPES.VEGETARIAN ||
+    foodDishType === MEAL_TYPES.DESSERT
+  )
+);
+if (foodDishType === MEAL_TYPES.MEAT) {
+  meatDishType = prompt(
+    "Каква порция по размер желаете? Малка, средна или голяма"
+  );
+
+  const MEAT_PORTION_SIZE = {
+    SMALL: "малка порция",
+    MEDIUM: "средна порция",
+    LARGE: "голяма порция",
+  };
+
+  if (meatDishType === MEAT_PORTION_SIZE.SMALL) {
+    alert(
+      "Вие поръчахте малка порция от 450 грама, благодарим Ви за поръчката."
+    );
+  } else if (meatDishType === MEAT_PORTION_SIZE.MEDIUM) {
+    alert(
+      "Вие поръчахте средна порция от 750 грама, благодарим Ви за поръчката."
+    );
+  } else if (meatDishType === MEAT_PORTION_SIZE.LARGE) {
+    alert(
+      "Вие поръчахте голяма порция от 950 грама, благодарим Ви за поръчката."
+    );
+  } else {
+    alert("Няма такава порция.");
+  }
+
+  const COOKING_LEVELS = {
+    RARE: "RARE",
+    MEDIUM: "MEDIUM",
+    WELL_DONE: "WELL_DONE",
+  };
+  do {
+    donenessLevel = prompt(
+      "До каква степен желаете да Ви е изпечено месото? RARE, MEDIUM или WELL DONE?"
+    );
+  } while (
+    !(
+      donenessLevel === COOKING_LEVELS.RARE ||
+      donenessLevel === COOKING_LEVELS.MEDIUM ||
+      donenessLevel === COOKING_LEVELS.WELL_DONE
+    )
+  );
+
+  // Check if can cook RARE
+  if (
+    !conditions.hasGasStove &&
+    conditions.hasElectricGrill &&
+    conditions.kitchenMinTemperature >= 36 &&
+    conditions.kitchenMaxTemperature <= 48 &&
+    conditions.hasKitchenHood &&
+    conditions.MEAT_PORTION_SIZE === SMALL
+  ) {
+    canCookRare = true;
+  }
+  // Check if can cook MEDIUM
+  else if (
+    conditions.hasConvectionOven ||
+    conditions.staffCount === 5 ||
+    conditions.portionSize === PORTION_SIZES.MEDIUM ||
+    conditions.chefName === "Иван"
+  ) {
+    canCookMedium = true;
+  }
+  // Check if can cook WELL DONE
+  else if (
+    (conditions.closingTime >= 22 ||
+      ["Петър", "Радо"].includes(conditions.sauceChef)) &&
+    (conditions.KITCHEN_AREA === 45 ||
+      conditions.fryingPanCollection === 3 ||
+      conditions.potCollection === 2)
+  ) {
+    canCookWellDone = true;
+  }
 }
-
-do {foodDishType = prompt("Какво ястие желаете да ви приготвя? Месно ястие, Вегетарианско ястие или Десерт")
-    
-} while (!(foodDishType === "Месно ястие" || foodDishType === "Вегетарианско ястие" || foodDishType === "Десерт"))
-if (foodDishType === "Месно ястие"){
-    meatPortionSize = prompt("Каква порция по размер желаете?")
-    if (meatPortionSize === "малка порция"){
-        alert("Вие поръчахте малка порция от 450 грама, благодарим Ви за поръчката.")
-    } else if (meatPortionSize === "средна порция"){
-        alert("Вие поръчахте средна порция от 750 грама, благодарим Ви за поръчката.")
-    } else if (meatPortionSize === "голяма порция"){
-        alert("Вие поръчахте голяма порция от 950 грама, благодарим Ви за поръчката.")
-    } else {
-        alert("Няма такава порция.")
-    }
-   
-    do {meatDonenessLevel = prompt("До каква степен желаете да Ви е изпечено месото? RARE, MEDIUM или WELL DONE?")
-    
-    } while (!(meatDonenessLevel === "RARE" || meatDonenessLevel === "MEDIUM" || meatDonenessLevel === "WELL DONE"))
-    if (meatDonenessLevel === "RARE"){
-        isGasStoveOwned = false
-        isElectricalGrillnOwned = true
-        kitchenTemperature = kitchenMinTemperature 
-         = 36
-        //  в кухнята няма газов котлон;
-        // в кухнята има електрическа скара;
-        // температурата на въздуха е в интервала от 36 до 48 градуса;
-        // има аспиратор;
-        // избрана е малка порция.
-    } elif (meatDonenessLevel === "MEDIUM"){
-        // За да можете да приготвите MEDIUM месо трябва,
-
-        // да е налице едно от следните условия:
-
-        // в кухнята има конвектомат;
-        // в кухнята има персонал от точно 5 човека;
-        // избрана е средна порция;
-        // името на главния готвач е Иван.
-    } elif (meatDonenessLevel === "WELL DONE"){
-        // За да можете да приготвите WELL DONE месо трябва, едно от тези условия да е изпълнено:
-
-        // кухнята работи до 22:00;
-        // отговорникът на сосовете се казва Петър или Радо. И едно от тези условия:
-        // кухнята е с квадратура 45 квадратни метра;
-        // в кухнята има точно 3 тигана или точно 2 тенджери
-    }
-    }
